30d8cc8c6e92891005a9f399f91aa2cb15eada61
Adalberto Andrade de Menezes Neto <141356540+AdalAMN@users.noreply.github.com>
2024-10-18 11:59:43 -0400

    Update gerencia.c

--- DicionarioHash/gerencia.c
+++ DicionarioHash/gerencia.c
@@ -1,1 +1,29 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "estrutura.h"
 
+int main(){
+    int controle = 1
+    t_hash* Fabrica = criar_hash(0.7);
+    while(controle != 0){
+        printf("\n---Menu---\n")
+        printf("Selecione 1 para inserir item");
+        printf("Selecione 2 para buscar item");
+        printf("Selecione 3 para remover item");
+        printf("Selecione 4 para atualizar estoque");
+        printf("Selecione 0 para fechar");
+        controle = int(input())
+        switch(controle){
+            case(1):
+
+            case(2):
+
+            case(3):
+
+            case(4):
+
+            case(5):
+            
+        }
+    }
+}
