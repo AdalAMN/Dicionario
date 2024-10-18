442f77df7c900dc338414574ca4f1546f8bf3042
Adalberto Andrade de Menezes Neto <141356540+AdalAMN@users.noreply.github.com>
2024-10-18 00:16:01 -0400

    Alterações nos tipos

--- DicionarioHash/estrutura.c
+++ DicionarioHash/estrutura.c
@@ -1,11 +1,17 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-typedef struct elem_hash t_elem_hash;
-struct elem_hash{
+typedef struct elem_hash{
     int chave;
     void* carga;
-};
+}t_elem_hash;
+
+typedef struct hash{
+    t_imprimir_hash impressora;
+    t_lse* *vetor;
+    int tamanho;
+    double fc;
+}t_hash;
 
 t_elem_hash* criar_elem_hash(int chave, void* carga){
     t_elem_hash* e = malloc(sizeof(t_elem_hash));
@@ -15,23 +21,23 @@ t_elem_hash* criar_elem_hash(int chave, void* carga){
     return e;
 }
 
-typedef struct sensores{
+struct sensores{
     char tipo;
     char localizacao;
     char dadosHistoricos;
     char alarmes;
-}t_sensores;
+};
 
-typedef struct equipamentos{
+struct equipamentos{
     char modelo;
     char fabricante;
     int dataDeInstalacao;
     char historico;
-}t_equipamentos;
+};
 
-typedef struct materiais{
+struct materiais{
     int lote;
     char fornecedor;
     int dataValidade;
     char localizacao;
-}t_materiais;
+};
